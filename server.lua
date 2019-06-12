71a96b3caf7dad9bd342d9250350fcd4aa230cf2
rhys19 <30848481+rhys19@users.noreply.github.com>
2019-06-11 23:04:46 -0500

    Update server.lua

--- server.lua
+++ server.lua
@@ -4,6 +4,7 @@ local TornadoDestination = nil
 local TornadoGirth = 8.0
 local ace_perm = "rhys19.tornado"
 local debug = false
+isAdmin = false
 
 AddEventHandler("tornado:summon", function()
     local start = x,y,z
@@ -46,16 +47,18 @@ AddEventHandler("tornado:summon_right_here", function(x,y,z)
 end)
 
 function ProcessAces()
-    if GetNumPlayerIndices() > 0 then -- don't do it when there aren't any players
+	if GetNumPlayerIndices() > 0 then -- don't do it when there aren't any players
         for i=0, GetNumPlayerIndices()-1 do -- loop through all players
             player = tonumber(GetPlayerFromIndex(i))
             Citizen.Wait(0)
             if IsPlayerAceAllowed(player, ace_perm) then
                 TriggerClientEvent("sendAcePermissionToClient", player, true)
+                TriggerEvent("admincheck", player, true)
                 if debug then print("[DEBUG][" .. GetCurrentResourceName() .. "] ^5Syncronising player aces, sending to client...^0") end
             end
         end
     end
+	
 end
 
 Citizen.CreateThread(function()
@@ -64,13 +67,18 @@ Citizen.CreateThread(function()
         Citizen.Wait(0) -- lets check every minute
     end
 end)
-
 AddEventHandler("onResourceStart", function(name)
     if name == GetCurrentResourceName() then
         ProcessAces()
         if debug then print("[DEBUG][" .. GetCurrentResourceName() .. "] ^6Resource [ " .. GetCurrentResourceName() .. " ] was (re)started, syncing aces to all players.^0") end
     end
 end)
+
+RegisterNetEvent("admincheck")
+AddEventHandler("admincheck", function(state)
+    isAdmin = state
+end)
+
 
 RegisterServerEvent("tornado:delete2")
 AddEventHandler("tornado:delete2", function()
@@ -93,7 +101,37 @@ RegisterCommand("tornado", function(source, args, raw)
 	end
 end)
 
-RegisterNetEvent("sendAcePermissionToClient")
-AddEventHandler("sendAcePermissionToClient", function(state)
-    isAdmin = state
+--Update Check
+
+local LatestVersion = ''; CurrentVersion = '2.1.2'
+local GithubResourceName = 'Tornado-Script-FiveM'
+local githubUsername = 'rhys19'
+local versionurl = "https://raw.githubusercontent.com/"..githubUsername.."/"..GithubResourceName.."/master/VERSION"
+local changesurl = "https://raw.githubusercontent.com/"..githubUsername.."/"..GithubResourceName.."/master/CHANGES"
+
+PerformHttpRequest(versionurl, function(Error, NewestVersion, Header)
+	PerformHttpRequest(changesurl, function(Error, Changes, Header)
+		LatestVersion = NewestVersion
+		print('\n')
+		print('====================================================================')
+		print(' Tornado ('..GetCurrentResourceName()..')')
+		print('====================================================================')
+		print(' Current Version: ' .. CurrentVersion.. ' ')
+		print(' Newest Version: ' .. NewestVersion.. ' ')
+		print('====================================================================')
+		if CurrentVersion ~= NewestVersion then
+			print(' Outdated ')
+			print(' Check the Github for new updates! ')
+			print('====================================================================')
+			print('CHANGES:\n' .. Changes)
+		if CurrentVersion > NewestVersion then
+		print("Your version is: "..CurrentVersion.." but it's higher then the updated version! Newest Version: "..NewestVersion)
+		end
+		else
+			print('============')
+			print(' Up to date!')
+			print('============')
+		end
+		print('\n')
+	end)
 end)
