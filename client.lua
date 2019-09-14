727cf852f9dcc426613171960c0f202d29338d21
Christopher M <10535902+cm8263@users.noreply.github.com>
2019-09-14 12:35:31 +1000

    Add files via upload

--- client.lua
+++ client.lua
@@ -1,22 +1,24 @@
 isAdmin = false
+local IsTornadoActive = false
 
 x,y,z = table.unpack(GetOffsetFromEntityInWorldCoords(PlayerPedId(), 0.1, 8.0, 1.0))
 heading = GetEntityHeading(PlayerPedId()+90, 1, 0)
 Citizen.CreateThread(function()
     local Script = MainScript:new()
     Script:MainScript()
-    local IsTornadoActive = false
+    IsTornadoActive = false
     local Tornado = nil
 
 
     RegisterNetEvent("tornado:spawn")
     AddEventHandler("tornado:spawn", function(pos, dest)
+        IsTornadoActive = true
+        print(IsTornadoActive)
         pos = vec3(x,y,z)
         dest = vec3(x,y,z)
         Tornado = Script._factory:CreateVortex(pos)
         Tornado._position = pos
         Tornado._destination = dest
-        IsTornadoActive = true
     end)
 
     RegisterNetEvent("tornado:setPosition")
@@ -56,9 +58,10 @@ end)
 Citizen.CreateThread(function()
     while true do
         Citizen.Wait(0) -- Wait 0 seconds to prevent crashing ;)
-    	if IsTornadoActive == true then -- checks if the tornado is active
-			TriggerServerEvent('InteractSound_SV:PlayOnAll', 'tornado', 1.0) -- if tornado is active then play tornado siren.
-		end
+    	if IsTornadoActive then -- checks if the tornado is active
+            TriggerEvent('LIFE_CL:Sound:PlayOnAll', 'demo', 1.0) -- if tornado is active then play tornado siren.
+            Citizen.Wait(1000)
+        end
     end
 end)
 
