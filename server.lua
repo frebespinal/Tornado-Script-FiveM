0b38668b099617f3673081c33bcdfea16595a9f3
rhys19 <30848481+rhys19@users.noreply.github.com>
2019-10-31 00:20:58 -0500

    Update server.lua

--- server.lua
+++ server.lua
@@ -2,9 +2,6 @@ local IsTornadoActive = false
 local TornadoPosition = nil
 local TornadoDestination = nil
 local TornadoGirth = 8.0
-local ace_perm = "rhys19.tornado"
-local debug = false
-isAdmin = false
 
 AddEventHandler("tornado:summon", function()
     local start = x,y,z
@@ -24,11 +21,9 @@ AddEventHandler("tornado:move_here", function(x,y,z)
         TornadoDestination = x,y,z, heading
         if not IsTornadoActive then
             TornadoPosition = x,y,z, heading
-            --print("[Tornado] A tornado has spawned at " .. TornadoPosition.x .. ", " .. TornadoPosition.y .. ", " .. TornadoPosition.z)
         end
         IsTornadoActive = true
         TriggerClientEvent("tornado:spawn", -1, TornadoPosition, TornadoDestination)
-        --print("[Tornado] A tornado is moving to " .. x .. ", " .. y .. ", " .. z)
     end
 end)
 
@@ -40,45 +35,9 @@ AddEventHandler("tornado:summon_right_here", function(x,y,z)
             TornadoDestination = x,y,z, heading
         end
         IsTornadoActive = true
-        TriggerClientEvent("tornado:spawn", -1, x,y,z, heading)
-        --print("[Tornado] A tornado has spawned at " .. x .. ", " .. y .. ", " .. z)
-		
+        TriggerClientEvent("tornado:spawn", -1, x,y,z, heading)		
     end
 end)
-
-function ProcessAces()
-	if GetNumPlayerIndices() > 0 then -- don't do it when there aren't any players
-        for i=0, GetNumPlayerIndices()-1 do -- loop through all players
-            player = tonumber(GetPlayerFromIndex(i))
-            Citizen.Wait(0)
-            if IsPlayerAceAllowed(player, ace_perm) then
-                TriggerClientEvent("sendAcePermissionToClient", player, true)
-                TriggerEvent("admincheck", player, true)
-                if debug then print("[DEBUG][" .. GetCurrentResourceName() .. "] ^5Syncronising player aces, sending to client...^0") end
-            end
-        end
-    end
-	
-end
-
---[[Citizen.CreateThread(function()
-    while true do
-        ProcessAces()
-        Citizen.Wait(0) -- lets check every minute
-    end
-end)--]]
-AddEventHandler("onResourceStart", function(name)
-    if name == GetCurrentResourceName() then
-        ProcessAces()
-        if debug then print("[DEBUG][" .. GetCurrentResourceName() .. "] ^6Resource [ " .. GetCurrentResourceName() .. " ] was (re)started, syncing aces to all players.^0") end
-    end
-end)
-
-RegisterNetEvent("admincheck")
-AddEventHandler("admincheck", function(state)
-    isAdmin = state
-end)
-
 
 RegisterServerEvent("tornado:delete2")
 AddEventHandler("tornado:delete2", function()
