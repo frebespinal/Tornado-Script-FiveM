d361d410bda806e23379e38743201418b8b86786
rhys19 <30848481+rhys19@users.noreply.github.com>
2021-08-02 22:58:41 -0500

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -7,22 +7,21 @@ description 'Tornado Script for fivem by rhys19'
 version '1.0.4'
 
 client_scripts {
+	'soundfx/client/main.lua',
     'helpers/GameSound.lua',
     'helpers/Helpers.lua',
     'helpers/LoopedParticle.lua',
     'helpers/MathEx.lua',
-    'Dependencies/NativeUI.lua',
-
     'tornado/TEntity.lua',
     'tornado/TFactory.lua',
     'tornado/TParticle.lua',
     'tornado/TScript.lua',
     'tornado/TVortex.lua',
-
     'client.lua'
 }
 
 server_scripts {
     'server.lua',
-    'version.lua'
+    'version.lua',
+    'soundfx/server/main.lua'
 }
