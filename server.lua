8ead3d0e689dc07111b34ce6ee36ffb518a21990
rhys19 <matrixgamingfivem@gmail.com>
2021-08-02 23:23:43 -0500

    Update to 2.3.1

---
+++ server.lua
@@ -0,0 +1,143 @@
+--FILE EDITED BY SPJESTER
+-- Note: New Wording in chat for active Tornado.
+local IsTornadoActive = false
+local TornadoPosition = nil
+local TornadoDestination = nil
+local TornadoGirth = 8.0
+local ace_perm = "rhys19.tornado"
+local debug = false
+isAdmin = false
+
+RegisterServerEvent("tornado:summon")
+AddEventHandler("tornado:summon", function()
+    local start = x,y,z
+    local destination = x,y,z
+    if start==destination then
+        destination = x,y,z
+    end
+    TornadoPosition = x,y,z, heading
+    TornadoDestination = x,y,z, heading
+    IsTornadoActive = true
+    TriggerClientEvent("tornado:spawn", -1, x,y,z, heading)
+ end)
+
+RegisterServerEvent("tornado:move_here")
+AddEventHandler("tornado:move_here", function(x,y,z)
+    if x~=nil and y~=nil and z~=nil then
+        TornadoDestination = x,y,z, heading
+        if not IsTornadoActive then
+            TornadoPosition = x,y,z, heading
+            print("[TORNADO WARNING!!] A tornado has been spotted at " .. TornadoPosition.x .. ", " .. TornadoPosition.y .. ", " .. TornadoPosition.z)
+        end
+        IsTornadoActive = true
+        TriggerClientEvent("tornado:spawn", -1, TornadoPosition, TornadoDestination)
+        print("[TORNADO WARNING!!] A tornado is moving towards " .. x .. ", " .. y .. ", " .. z)
+    end
+end)
+
+RegisterServerEvent("tornado:summon_right_her")
+AddEventHandler("tornado:summon_right_here", function(x,y,z)
+	  x,y,z = table.unpack(GetOffsetFromEntityInWorldCoords(PlayerPedId(source), 0.0, 8.0, 0.5))
+    if x~=nil and y~=nil and z~=nil then
+        TornadoPosition = x,y,z, heading
+        if not IsTornadoActive then
+            TornadoDestination = x,y,z, heading
+        end
+        IsTornadoActive = true
+        TriggerClientEvent("tornado:spawn", -1, x,y,z, heading)
+        --print("[Tornado] A tornado has spawned at " .. x .. ", " .. y .. ", " .. z)
+		
+    end
+end)
+
+function ProcessAces()
+	if GetNumPlayerIndices() > 0 then -- don't do it when there aren't any players
+        for i=0, GetNumPlayerIndices()-1 do -- loop through all players
+            player = tonumber(GetPlayerFromIndex(i))
+            Citizen.Wait(0)
+            if IsPlayerAceAllowed(player, ace_perm) then
+                TriggerClientEvent("sendAcePermissionToClient", player, true)
+                TriggerEvent("admincheck", player, true)
+                if debug then print("[DEBUG][" .. GetCurrentResourceName() .. "] ^5Syncronising player aces, sending to client...^0") end
+            end
+        end
+    end
+	
+end
+
+--[[Citizen.CreateThread(function()
+    while true do
+        ProcessAces()
+        Citizen.Wait(0) -- lets check every minute
+    end
+end)--]]
+AddEventHandler("onResourceStart", function(name)
+    if name == GetCurrentResourceName() then
+        ProcessAces()
+        if debug then print("[DEBUG][" .. GetCurrentResourceName() .. "] ^6Resource [ " .. GetCurrentResourceName() .. " ] was (re)started, syncing aces to all players.^0") end
+    end
+end)
+
+RegisterNetEvent("admincheck")
+AddEventHandler("admincheck", function(state)
+    isAdmin = state
+end)
+
+
+RegisterServerEvent("tornado:delete2")
+AddEventHandler("tornado:delete2", function()
+    IsTornadoActive = false
+    TriggerClientEvent("tornado:delete", -1)
+    print("^2[NATIONAL WEATHER SERVICE] ^0The Tornado has now went back into the clouds!")
+end)
+
+RegisterServerEvent("tornado:delete")
+AddEventHandler("tornado:delete", function()
+    IsTornadoActive = false
+    TriggerClientEvent("tornado:delete", -1)
+end)
+
+RegisterCommand("tornado", function(source, args, raw)
+	if (args[1] == "summon") then
+    TriggerEvent("tornado:summon")
+	elseif (args[1] == "delete") then
+	TriggerEvent("tornado:delete")
+	elseif #args < 1 then
+	return TriggerClientEvent('chat:addMessage', source, { color = { 255, 0, 0}, multiline = true, args = {"^1System", "Invalid Arguments!"} })
+	end
+end)
+
+--Update Check
+
+local LatestVersion = ''; CurrentVersion = '2.1.2'
+local GithubResourceName = 'Tornado-Script-FiveM'
+local githubUsername = 'rhys19'
+local versionurl = "https://raw.githubusercontent.com/"..githubUsername.."/"..GithubResourceName.."/master/VERSION"
+local changesurl = "https://raw.githubusercontent.com/"..githubUsername.."/"..GithubResourceName.."/master/CHANGES"
+
+PerformHttpRequest(versionurl, function(Error, NewestVersion, Header)
+	PerformHttpRequest(changesurl, function(Error, Changes, Header)
+		LatestVersion = NewestVersion
+		print('\n')
+		print('====================================================================')
+		print(' Tornado: '..GetCurrentResourceName()..' ')
+		print('====================================================================')
+		print(' Current Version: ' .. CurrentVersion.. ' ')
+		print(' Newest Version: ' .. NewestVersion.. ' ')
+		print('====================================================================')
+		if CurrentVersion ~= NewestVersion then
+			print(' Outdated ')
+			print(' Check the Github for new updates! ')
+			print('====================================================================')
+			print('CHANGES:\n' .. Changes)
+		if CurrentVersion > NewestVersion then
+		print("Your version is: "..CurrentVersion.." but it's higher then the updated version! Newest Version: "..NewestVersion)
+		end
+		else
+			print('============')
+			print(' Up to date!')
+			print('============')
+		end
+		print('\n')
+	end)
+end)
