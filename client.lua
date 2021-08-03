178afe545cc8f96701a69520b1bb841f16d344f2
rhys19 <30848481+rhys19@users.noreply.github.com>
2021-08-02 22:57:59 -0500

    Update client.lua

--- client.lua
+++ client.lua
@@ -1,7 +1,9 @@
+--FILE EDITED BY SPJESTER
+-- Note: Custom Sound System for Tornado, Added Siren Sound
 local IsTornadoActive = false
 
-x,y,z = table.unpack(GetOffsetFromEntityInWorldCoords(PlayerPedId(), 0.1, 8.0, 1.0))
-heading = GetEntityHeading(PlayerPedId()+90, 1, 0)
+x,y,z = table.unpack(GetOffsetFromEntityInWorldCoords(PlayerPedId(source), 5.0, 8.0, 1.0))
+heading = GetEntityHeading(PlayerPedId(source)+90, 1, 0)
 Citizen.CreateThread(function()
     local Script = MainScript:new()
     Script:MainScript()
@@ -11,8 +13,12 @@ Citizen.CreateThread(function()
 
     RegisterNetEvent("tornado:spawn")
     AddEventHandler("tornado:spawn", function(pos, dest)
+	TriggerServerEvent("alert:sv", "National Weather Service", "A tornado on the ground has been spotted! Get Indoors! Seek shelter immediately!!!")
+	TriggerEvent('chat:addMessage', { color = { 255, 0, 0}, multiline = true, args = {"^2[NATIONAL WEATHER SERVICE]", "^1A tornado on the ground has been spotted!"} })
+	TriggerEvent('chat:addMessage', { color = { 255, 0, 0}, multiline = true, args = {"^3[TORNADO WARNING!!]", "^5Get Indoors! Seek shelter immediately!!!"} })
         IsTornadoActive = true
-        pos = vec3(x,y,z)
+		TriggerEvent('TORNADO_CL:Sound:PlayOnAll', 'siren', 0.5) -- if tornado is active then play tornado siren.
+		pos = vec3(x,y,z)
         dest = vec3(x,y,z)
         Tornado = Script._factory:CreateVortex(pos)
         Tornado._position = pos
@@ -35,15 +41,17 @@ Citizen.CreateThread(function()
 
     RegisterNetEvent("tornado:delete")
     AddEventHandler("tornado:delete", function()
+	TriggerServerEvent("alert:sv", "National Weather Service", "The Tornado Warning has expired, the tornado has dissipated")
+	TriggerEvent('chat:addMessage', { color = { 255, 0, 0}, multiline = true, args = {"^2[NATIONAL WEATHER SERVICE]", "^0The Tornado has now went back into the clouds!"} })
         IsTornadoActive = false
     end)
 
     while true do
         if IsTornadoActive and Tornado then
-            Script:OnUpdate(GetGameTimer())
+		Script:OnUpdate(GetGameTimer())
         else
-            if Tornado then
-                Tornado._position = vec3(10000.0, 10000.0, 0.0)
+            if Tornado then		
+				Tornado._position = vec3(10000.0, 10000.0, 0.0)
                 Script:OnUpdate(GetGameTimer())
                 Tornado = nil
             end
@@ -52,13 +60,10 @@ Citizen.CreateThread(function()
     end
 
 end)
+--[[
 
-Citizen.CreateThread(function()
-    while true do
-        Citizen.Wait(0) -- Wait 0 seconds to prevent crashing ;)
-    	if IsTornadoActive then -- checks if the tornado is active
-            TriggerEvent('LIFE_CL:Sound:PlayOnAll', 'demo', 1.0) -- if tornado is active then play tornado siren.
-            Citizen.Wait(2500)
-        end
-    end
+RegisterNetEvent("sendAcePermissionToClient")
+AddEventHandler("sendAcePermissionToClient", function(state)
+    isAdmin = state
 end)
+]]
